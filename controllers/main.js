18715062c854e2cfeabb92f5f35150316d1b2478
Giles Smart <giles.smart@webapplicationsuk.com>
2014-10-30 21:14:51 +0000

    Renamed some variables.

--- controllers/main.js
+++ controllers/main.js
@@ -1,19 +1,19 @@
 angular.module('formatter')
     .controller('Main', ['$scope', 'XmlParser', 'JsonParser', function ($scope, xmlParser, jsonParser) {
         $scope.model = {
-            indentationLevel: 4
+            indentSize: 4
         };
 
         $scope.$watch('model.input', updateOutput);
-        $scope.$watch('model.indentationLevel', updateOutput);
+        $scope.$watch('model.indentSize', updateOutput);
 
         function updateOutput() {
             var parseTree;
 
-            if ($scope.model.input && $scope.model.indentationLevel) {
+            if ($scope.model.input && $scope.model.indentSize) {
                 if (parseTree = xmlParser.parse($scope.model.input) || jsonParser.parse($scope.model.input)) {
                     console.log(parseTree);
-                    $scope.model.output =  parseTree.getFormattedString($scope.model.indentationLevel);
+                    $scope.model.output =  parseTree.getFormattedString($scope.model.indentSize);
                     $scope.model.errorMessage = null;
                 }
                 else {
