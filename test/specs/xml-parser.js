aa00b7f50a4aa1f5f3f576b0327b66f6344b95a3
Giles Smart <gilessmat@users.noreply.github.com>
2016-04-02 18:15:47 +0100

    Resolved known bugs in XML formatting for Firefox and Chrome.

--- test/specs/xml-parser.js
+++ test/specs/xml-parser.js
@@ -9,6 +9,33 @@ describe('xml-parser', function () {
         indentGenerator = new formatter.services.IndentGenerator();
         xmlGenerator = new formatter.services.XmlGenerator(indentGenerator);
         xmlParser = new formatter.services.XmlParser(xmlGenerator);
+    });    
+    
+    it('writes empty elements', function () {
+        var input = '<root></root>',
+            expectedOutput = '<root />',
+            indentAmount = 1,
+            output = xmlParser.parse(input).getFormattedString(indentAmount);
+        
+        expect(output).toBe(expectedOutput);
+    });
+    
+    it('writes elements with single line text content on one line', function () {
+        var input = '<root>\ntest\n</root>',
+            expectedOutput = '<root>test</root>',
+            indentAmount = 1,
+            output = xmlParser.parse(input).getFormattedString(indentAmount);
+        
+        expect(output).toBe(expectedOutput);
+    });
+    
+    it('writes comments', function () {
+        var input = '<root><!-- test --></root>',
+            expectedOutput = '<root>\n <!-- test -->\n</root>',
+            indentAmount = 1,
+            output = xmlParser.parse(input).getFormattedString(indentAmount);
+        
+        expect(output).toBe(expectedOutput);
     });
     
     it('writes empty comments', function () {
@@ -20,9 +47,36 @@ describe('xml-parser', function () {
         expect(output).toBe(expectedOutput);
     });
     
+    it('writes CDATA', function () {
+        var input = '<root><![CDATA[test]]></root>',
+            expectedOutput = '<root>\n <![CDATA[test]]>\n</root>',
+            indentAmount = 1,
+            output = xmlParser.parse(input).getFormattedString(indentAmount);
+        
+        expect(output).toBe(expectedOutput);
+    });
+    
     it('writes processing instructions', function () {
+        var input = '<root><?name content?></root>',
+            expectedOutput = '<root>\n <?name content ?>\n</root>',
+            indentAmount = 1,
+            output = xmlParser.parse(input).getFormattedString(indentAmount);
+        
+        expect(output).toBe(expectedOutput);
+    });
+    
+    it('writes stylesheet processing instructions', function () {
         var input = '<root><?xml-stylesheet type="text/xsl" href="style.xsl" ?></root>',
             expectedOutput = '<root>\n <?xml-stylesheet type="text/xsl" href="style.xsl" ?>\n</root>',
+            indentAmount = 1,
+            output = xmlParser.parse(input).getFormattedString(indentAmount);
+        
+        expect(output).toBe(expectedOutput);
+    });
+    
+    it('does not write empty text nodes between child nodes', function () {
+        var input = '<root> <test1 /> <test2 /> </root>',
+            expectedOutput = '<root>\n <test1 />\n <test2 />\n</root>',
             indentAmount = 1,
             output = xmlParser.parse(input).getFormattedString(indentAmount);
         
